480561ec0dace6d79ed39ba7ee66ce1490c83e38
francescoceschel <francescoceschel@gmail.com>
2026-02-26 05:45:27 -0500

    commit up to task 7

--- server.py
+++ server.py
@@ -9,14 +9,14 @@ def emo_detector():
     text_to_analyze = request.args.get('textToAnalyze')
     result = emotion_detector(text_to_analyze)
 
+    if result['dominant_emotion'] is None:
+        return 'Invalid text! Please try again!'
     anger = result['anger']
     disgust = result['disgust']
     fear = result['fear']
     joy = result['joy']
     sadness = result['sadness']
-    #dominant_emotion = max(result, key = result.get)
     dominant_emotion = result['dominant_emotion']
-
     return f'For the given statement, the system response is "anger": {anger}, "disgust": {disgust}, "fear": {fear}, "joy": {joy}, and "sadness": {sadness}. The dominant emotion is {dominant_emotion}'
 
 @app.route("/")
